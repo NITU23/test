92cc87b290eb5d7ac9bb4a69bc503c92c3aa391e
Nitin vyas <nitinvyas200@gmail.com>
2023-09-25 11:13:36 +0530

    test project finished

--- Project/src/App.js
+++ Project/src/App.js
@@ -1,8 +1,22 @@
 import Navbar from "./components/Navbar";
+import Form from "./components/Form"
+import Alert from "./components/Alert";
+import { useState } from "react";
 function App() {
+  const [alert,setAlert] = useState({msg:'Welcome',type:'success'});
+  const showAlert = (message,type) => {
+    setAlert ({
+      msg : message,
+      type : type
+    })
+  }
   return (
     <div >
-     <Navbar title='Navbar2' />
+     <Navbar title='TextUtil' />
+     <Alert  alert={alert} />
+     <div className="container my-3">
+     <Form heading="Enter the text below" showAlert= {showAlert} />
+     </div>
     </div>
   );
 }
--- Project/src/components/Form.js
+++ Project/src/components/Form.js
@@ -1,16 +1,48 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
-export class Form extends Component {
-  render() {
-    return (
-      <div>
-            <div class="mb-3">
-                <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-            </div>
-      </div>
-    )
+const Form = (props) => {
+  const [text, setText] = useState('')
+  const handleOnChange = (event) => {
+    setText(event.target.value)
   }
+  const convertupCase = () => {
+    let newText = text.toUpperCase();
+    setText(newText)
+    props.showAlert('Converted to upper case', 'success')
+    setTimeout(() => {
+      props.showAlert(null)
+    }, 2000)
+  }
+  const convertLowerCase = () => {
+    let newText = text.toLowerCase();
+    setText(newText)
+    props.showAlert('Converted to Lower case', 'success')
+    setTimeout(() => {
+      props.showAlert(null)
+    }, 2000)
+  }
+  const handleClear = () => {
+    setText('')
+    props.showAlert('Text cleared', 'success')
+    setTimeout(() => {
+      props.showAlert(null)
+    }, 2000)
+
+  }
+  return (
+    <div>
+      <div className=" container mb-3">
+        <h1>{props.heading}</h1>
+        <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="8"></textarea>
+      </div>
+      <button className='btn btn-primary mx-2' onClick={convertupCase} >Convert to Upper Case</button>
+      <button className='btn btn-primary mx-2' onClick={convertLowerCase} >Convert to Lower Case</button>
+      <button className='btn btn-primary mx-2' onClick={handleClear} >Clear Text</button>
+      <div className='container my-2'>
+        <p>hello you have written {text.split(" ").length} words and {text.length} characters</p>
+      </div>
+    </div>
+  )
 }
 
 export default Form
