221eb866eaafbb102f9b4a151d8ba6e445600bee
Nitin vyas <nitinvyas200@gmail.com>
2023-09-30 22:18:16 +0530

    newsapp changes and api called

--- project2/src/components/News.js
+++ project2/src/components/News.js
@@ -1,30 +1,71 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
-
+import Spinner from './Spinner';
+import PropTypes from 'prop-types'
 export default class News extends Component {
+  static defaultProps = {
+     country : 'in',
+     pageSize : 8,
+     category : 'general'
+     
+  }
+  static propTypes = {
+    country : PropTypes.string,
+    pageSize : PropTypes.number,
+    category : PropTypes.string
+  }
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1
+    }
+  }
+  async updateNews () {
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=b7821a64bf974bcba4ec36a6264ae09e&page=${this.state.page}&pageSize=${this.props.pageSize}`
+    this.setState({ loading: true })
+    let data = await fetch(url)
+    let parsedData = await data.json()
+    this.setState({
+      articles: parsedData.articles,
+      totalResult: parsedData.totalResults,
+      loading: false
+    })
+  }
+  async componentDidMount() {
+    this.updateNews();
+
+  }
+  handleNextClick = async () => {
+    this.setState({page:this.state.page+1})
+    this.updateNews()
+  }
+  handlePrevClick = async () => {
+    this.setState({page:this.state.page+1})
+    this.updateNews()
+  }
   render() {
     return (<>
-      <div className='container my-3'> 
-      <h1>News App - Top headlines</h1>
-      <div className='row'>
-        <div className='col-md-4'>
-        <NewsItem title='newTitle' description='description'/>
+      <div className='container my-3'>
+
+        <h1 className='text-center'>News App - Top headlines</h1>
+        {this.state.loading && <Spinner />}
+        <div className='row'>
+          {
+            !this.state.loading && this.state.articles.map((element) => {
+              return <div className='col-md-4 my-4' key={element.imageUrl}>
+                <NewsItem title={element.title ? element.title.slice(0, 40) : ""} description={element.description ? element.description.slice(0, 80) : ""} imageUrl={element.urlToImage} newsUrl={element.url} 
+                author = {element.author} date={element.publishedAt}/>
+              </div>
+            })}
         </div>
-        <div className='col-md-4'>
-        <NewsItem title='newTitle' description='description'/>
-        </div>
-        <div className='col-md-4'>
-        <NewsItem title='newTitle' description='description'/>
-        </div>
-        <div className='col-md-4'>
-        <NewsItem title='newTitle' description='description'/>
-        </div>
-        <div className='col-md-4'>
-        <NewsItem title='newTitle' description='description'/>
+        <div className='container d-flex justify-content-between'>
+          <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+          <button type="button" disabled={this.state.page + 1 > Math.ceil(this.state.totalResult / this.props.pageSize)} className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
       </div>
-      </div>
-      </>
+    </>
     )
   }
 }
--- project2/src/components/NewsItem.js
+++ project2/src/components/NewsItem.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
-    render(orops) {
-        let {title,description} = this.props
+    
+    render(props) {
+        let {title,description,imageUrl,newsUrl,author,date} = this.props
         return (
             <div>
-                <div className="card" style={{width: "18rem"}} >
-                    <img src="..." className="card-img-top" alt="..." />
+                <div className="card " style={{width: "18rem"}} >
+                    <img src={!imageUrl ? 'https://image.cnbcfm.com/api/v1/image/107223512-1681243273349-gettyimages-1481446098-dji_0046_mtqhg3nn.jpeg?v=1695903858&w=1920&h=1080':imageUrl } className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">{title}</h5>
-                        <p className="card-text">{description}</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                        <h5 className="card-title">{title}...</h5>
+                        <p className="card-text">{description}...</p>
+                        <p className="card-text"><small className="text-muted">By {author ?author:"unknown"} on {new Date(date).toGMTString()}</small></p>
+                        <a href={newsUrl} rel="noreferrer" target='_blank' className="btn btn-sm btn-dark">ReadMore</a>
                     </div>
                 </div>
             </div>
